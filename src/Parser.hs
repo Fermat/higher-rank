5a7e9d11fd836b75f28ad0820c5d764e8bd1f826
Frank Fu <peng.frank.fu@gmail.com>
2017-06-19 13:35:59 -0300

    add templates

---
+++ src/Parser.hs
@@ -0,0 +1,274 @@
+{-#LANGUAGE PackageImports, StandaloneDeriving, DeriveDataTypeable, FlexibleContexts#-}
+module Fcr.Parser where
+import Fcr.Syntax
+import Fcr.PrettyPrinting
+
+import Text.Parsec hiding (ParseError,Empty, State)
+import qualified Text.Parsec as P
+import Text.Parsec.Language
+import Text.Parsec.Char
+import Text.Parsec.Expr(Operator(..),Assoc(..),buildExpressionParser)
+import qualified Text.Parsec.Token as Token
+import Text.Parsec.Indent
+import Control.Applicative hiding ((<|>),many, optional, Const)
+import Control.Monad.State.Lazy
+import "mtl" Control.Monad.Identity
+import Control.Exception(Exception)
+
+import qualified Data.IntMap as IM
+import Data.Typeable
+import Data.Char
+import Data.List
+
+parseModule :: String -> String -> Either P.ParseError Module
+parseModule srcName cnts =
+ runIndent $ runParserT gModule () srcName cnts
+
+parseExp :: String -> Either P.ParseError Exp
+parseExp s = runIndent $ runParserT (try (parens term) <|> term) () [] s
+
+-- parseExps :: String -> Either P.ParseError [Exp]
+-- parseExps s = runIndent [] $ runParserT (many1 (try (parens term) <|> term)) () [] s
+
+type Parser a = IndentParser String () a
+
+-- deriving instance Typeable P.ParseError
+-- instance Exception P.ParseError 
+
+-- parse module
+gModule :: Parser Module
+gModule = do
+  bs <- many (try ruleDecl)
+  ds <- many (try decl)
+  qs <- many (try proof)
+  sts <- many (try step)
+  eof
+  return $ Mod bs qs ds sts
+
+
+decl :: Parser (Name, Exp, Exp)
+decl = do
+  n <- identifier
+  when (isUpper (head n)) $ parserFail "expected to begin with lowercase letter"
+  reservedOp ":"
+  formula <- term
+  n' <- identifier
+  when (n /= n') $ parserFail ("unexpected definition" ++ n')
+  vs <- many var
+  reservedOp "="
+  b <- term
+  let b' = convert b
+      bb = expand b'
+      exp = foldr (\ x y -> Lambda x Nothing y) bb (map (\(Var x) -> x) vs)
+  return $ (n, formula, exp)    
+      
+  
+proof :: Parser ((Name, Exp), [Tactic])
+proof = do
+  reserved "lemma"
+  n <- identifier
+  reservedOp ":"
+  t <- term
+  reserved "proof"
+  ts <- many tactic
+  reserved "qed"
+  return ((n, t), ts)
+
+tactic :: Parser Tactic
+tactic = tacIntros <|> tacApply <|> tacUse <|> tacCoind <|> tacApplyH
+
+tacIntros = do
+  reserved "intros"
+  ns <- many1 identifier
+  return $ Intros ns
+
+tacCoind = reserved "coind" >> return Coind
+
+tacUse = do
+  reserved "use"
+  n <- identifier
+  ts <- optionMaybe term
+  case ts of
+    Nothing -> return $ Use n []
+    Just ts' -> return $ Use n (flatten ts')
+
+tacApply = do
+  reserved "apply"
+  n <- identifier
+  ts <- optionMaybe term
+  case ts of
+    Nothing -> return $ Apply n []
+    Just ts' -> return $ Apply n (flatten ts')
+
+tacApplyH = do
+  reserved "applyh"
+  n <- identifier
+  return $ ApplyH n 
+  
+step :: Parser (Name, Int)
+step = do
+  reserved "step"
+  n <- identifier
+  num <- integer
+  return (n, fromIntegral num)
+
+    
+ruleDecl :: Parser (Name, Exp)
+ruleDecl = do
+  (Const c) <- con 
+  reservedOp ":"
+  t <- term
+  return $ (c, t)
+  
+var :: Parser Exp
+var = do
+  n <- identifier
+  when (isUpper (head n)) $ parserFail "expected to begin with lowercase letter"
+  return (Var n)
+
+con :: Parser Exp
+con = do
+  n <- identifier  
+  when (isLower (head n)) $ parserFail "expected to begin with uppercase letter"
+  return (Const n)
+
+eigen :: Parser Exp
+eigen = do
+  n <- brackets identifier  
+  when (isUpper (head n)) $ parserFail "expected to begin with lowercase letter"
+  return (Const n)
+
+-- parser for FType--
+-- rule :: Parser Exp
+-- rule = do
+--   t1 <- term
+--   reserved "->"
+--   t2 <- term
+--   return $ Arrow t1 t2
+
+term :: Parser Exp
+term = buildExpressionParser typeOpTable base
+
+-- base :: Parser Exp
+-- base = try compound <|> try forall <|> parens ftype
+
+-- binOp :: Assoc -> String -> (a -> a -> a) -> Operator String u (State SourcePos) a
+binOp assoc op f = Infix (reservedOp op >> return f) assoc
+
+-- typeOpTable :: [[Operator String u (State SourcePos) Exp]]
+typeOpTable = [[binOp AssocRight "=>" Imply, binOp AssocRight "<=" Arrow]]
+
+-- parse type expression
+base :: Parser Exp
+base = forall <|> lambda <|> try compound <|> try (parens term)
+
+lambda = do
+  reservedOp "\\"
+  as <- many1 var
+  reservedOp "."
+  p <- term
+  return $ foldr (\ x y -> Abs x y) p (map (\(Var x) -> x) as)
+
+forall = do
+  reserved "forall"
+  as <- many1 var
+  reservedOp "."
+  p <- term
+  return $ foldr (\ x y -> Forall x y) p (map (\(Var x) -> x) as)
+
+compound = do
+  n <- eigen <|> try var <|> con <|> parens term 
+  as <- compoundArgs
+  if null as then return n
+    else return $ foldl' (\ z x -> PApp z x) n as 
+
+compoundArgs =
+  many $ indented >> (try eigen <|> try con <|> try var <|> try (parens term))
+
+
+-----------------------Positions -------
+  
+-- wrapPos :: Parser Exp -> Parser Exp
+-- wrapPos p = pos <$> getPosition <*> p
+--   where pos x (Pos y e) | x==y = (Pos y e)
+--         pos x y = Pos x y
+
+
+-------------------------------
+
+-- Tokenizer definition
+
+gottlobStyle :: (Stream s m Char, Monad m) => Token.GenLanguageDef s u m
+gottlobStyle = Token.LanguageDef
+                { Token.commentStart   = "{-"
+                , Token.commentEnd     = "-}"
+                , Token.commentLine    = "--"
+                , Token.nestedComments = True
+                , Token.identStart     = letter
+                , Token.identLetter    = alphaNum <|> oneOf "_'"
+                , Token.opStart        = oneOf ":!#$%&*+.,/<=>?@\\^|-"
+                , Token.opLetter       = (oneOf ":!#$%&*+.,/<=>?@\\^|-") <|> alphaNum
+                , Token.caseSensitive  = True
+                , Token.reservedNames =
+                  [
+                    "forall", "iota", "reduce", 
+                    "coind","use", "intros", "apply", "applyh",
+                    "by", "from", "in", "let", "simpCmp", "step",
+                    "case", "of",
+                    "data", "if", "then", "else",
+                    "axiom", "proof", "qed", "lemma", "auto",
+                    "show",
+                    "where", "module",
+                    "infix", "infixl", "infixr", "pre", "post",
+                    "formula", "prog", "set",
+                    "tactic", "deriving", "Ind"
+                  ]
+               , Token.reservedOpNames =
+                    ["\\", "->", "<=", ".","=", "::", ":", "=>"]
+                }
+
+-- tokenizer :: Token.GenTokenParser String u (State SourcePos)
+tokenizer = Token.makeTokenParser gottlobStyle
+
+-- identifier :: Parser String
+identifier = Token.identifier tokenizer
+
+-- whiteSpace :: ParsecT String u (State SourcePos) ()
+whiteSpace = Token.whiteSpace tokenizer
+
+-- reserved :: String -> ParsecT String u (State SourcePos) ()
+reserved = Token.reserved tokenizer
+
+-- reservedOp :: String -> ParsecT String u (State SourcePos) ()
+reservedOp = Token.reservedOp tokenizer
+
+-- operator :: ParsecT String u (State SourcePos) String
+operator = Token.operator tokenizer
+
+-- colon :: ParsecT String u (State SourcePos) String
+colon = Token.colon tokenizer
+
+-- integer :: ParsecT String u (State SourcePos) Integer
+integer = Token.integer tokenizer
+
+-- brackets :: ParsecT String u (State SourcePos) a -> ParsecT String u (State SourcePos) a
+brackets = Token.brackets tokenizer
+
+-- parens :: ParsecT String u (State SourcePos) a -> ParsecT String u (State SourcePos) a
+parens  = Token.parens tokenizer
+
+-- braces :: ParsecT String u (State SourcePos) a -> ParsecT String u (State SourcePos) a
+braces = Token.braces tokenizer
+
+-- dot :: ParsecT String u (State SourcePos) String
+dot = Token.dot tokenizer
+
+-- -- commaSep1 :: ParsecT String u (State SourcePos) a -> ParsecT String u (State SourcePos) [a]
+-- commaSep1 = Token.commaSep1 tokenizer
+
+-- --semiSep1 :: ParsecT String u (State SourcePos) a -> ParsecT String u (State SourcePos) [a]
+-- semiSep1 = Token.semiSep1 tokenizer
+
+-- comma :: ParsecT String u (State SourcePos) String
+comma = Token.comma tokenizer
+
---
+++ src/Syntax.hs
@@ -0,0 +1,355 @@
+module Syntax where
+
+import Control.Monad.State.Lazy
+import Control.Monad.Reader
+
+import Data.Char
+import qualified Data.Set as S
+import Data.List hiding (partition)
+import Debug.Trace
+
+type Name = String
+
+
+-- Variable convention: upper-case word represents constant and
+-- lower-case word represents variable, lower-case constant represent eigenvariable
+
+data Exp = Var Name
+          | Const Name
+          | App Exp Exp
+          | TApp Exp Exp
+          | PApp Exp Exp
+          | Lambda Name (Maybe Exp) Exp
+          | Abs Name Exp
+          | Imply Exp Exp
+          | Forall Name Exp
+          deriving (Show, Eq, Ord)
+
+data Kind = Star
+          | KVar Name
+          | KArrow Kind Kind
+          deriving (Show, Eq)
+
+-- nameless for the type
+data Nameless = V Int
+              | C Name
+              | IMP Nameless Nameless
+              | ALL Nameless
+              | AP Nameless Nameless
+              | LAM Nameless
+             deriving (Show, Eq)
+
+                     
+data Module = Mod {decls :: [(Name, Exp)] ,
+                   prfs :: [((Name, Exp), [Tactic])],
+                   pfDecl ::[(Name, Exp, Exp)],
+                   modsteps :: [(Name, Int)]}
+            deriving (Show)
+
+getRules ls = [(n, Arrow t1 t2) | (n, Arrow t1 t2) <- ls]
+getAxioms :: [(Name, Exp)] -> [(Name, Exp)]
+getAxioms ls = do
+  (n, t) <- ls
+  case t of
+    Arrow _ _ -> []
+    a -> return (n, a)
+toFormula :: [(Name, Exp)] -> [(Name, Exp)]
+toFormula env = map (\(n,e)-> (n, helper e)) env
+   where helper a@(Arrow t t') =
+           let vars = "p" : free a in
+           foldr (\ z x -> Forall z x) (Imply (PApp (Var "p") t') (PApp (Var "p") t)) vars
+         helper a@(Imply t t') =
+           let vars = free a in
+           foldr (\ z x -> Forall z x) a vars
+         helper a = a
+
+           
+convert :: Exp -> Exp
+convert (Var x) = (Var x)
+convert (Const x) = (Const x)
+convert (PApp f1 f2) = App (convert f1) (convert f2)
+convert (Abs x f) = Lambda x Nothing (convert f)
+
+
+-- A hack to get the 'reasonable' eta-long form.
+-- can easily broke with complicated situation, but it is
+-- safe in the sense that operationally it is fine because
+-- nothing can happen with eta expansion. But it may affect the
+-- reasoning though. Welp, decided not to be too clever in this case.
+expand :: Exp -> Exp
+expand b = b
+{-
+expand a@(Var x) = a
+expand a@(Const x) = Lambda "v'" Nothing (App a (Var "v'"))
+expand (Lambda x Nothing t) = Lambda x Nothing (expand t)
+expand (App (Var x) p) =  App (Var x) (expand p)
+expand (App (Const x) p) = App (Const x) (expand p)
+expand a@(App p1 p2) =
+  case flatten a of
+    (Var v): xs -> 
+      let res = map expand' xs
+      in reApp ((Var v): res)
+    b -> reApp b     
+
+
+expand' a@(Var x) =  Lambda "v'" Nothing (App a (Var "v'"))
+expand' a@(Const x) = Lambda "v'" Nothing (App a (Var "v'"))
+expand' (Lambda x Nothing t) = Lambda x Nothing (expand t)
+expand' (App (Var x) p) =  App (Var x) (expand p)
+expand' (App (Const x) p) = App (Const x) (expand p)
+expand' a@(App p1 p2) =
+  case flatten a of
+    (Var v): xs -> 
+      let res = map expand' xs
+      in reApp ((Var v): res)
+-}
+-- free vars of exp
+free = S.toList . freeVar 
+-- freeVar :: Exp -> [Name]
+freeVar (Var x) =  S.insert x S.empty
+freeVar (Const x) = S.empty
+-- freeVar (Const x) = if isLower (head x) then S.insert x S.empty else S.empty
+freeVar (Arrow f1 f2) = (freeVar f1) `S.union` (freeVar f2)
+freeVar (App f1 f2) = (freeVar f1) `S.union` (freeVar f2)
+freeVar (TApp f1 f2) = (freeVar f1) `S.union` (freeVar f2)
+freeVar (PApp f1 f2) = (freeVar f1) `S.union` (freeVar f2)
+freeVar (Forall x f) = S.delete x (freeVar f)
+freeVar (Lambda x (Just f1) f) =
+  S.delete x $ freeVar f `S.union` freeVar f1
+freeVar (Lambda x Nothing f) =
+  S.delete x $ freeVar f   
+freeVar (Abs x f) = S.delete x (freeVar f)
+freeVar (Imply b h) = freeVar b `S.union` freeVar h
+
+
+free' = S.toList . freeVar'
+-- freeVar :: Exp -> [Name]
+freeVar' (Var x) =  S.insert x S.empty
+freeVar' (Const x) = if isLower (head x) then S.insert x S.empty else S.empty
+freeVar' (Arrow f1 f2) = (freeVar' f1) `S.union` (freeVar' f2)
+freeVar' (App f1 f2) = (freeVar' f1) `S.union` (freeVar' f2)
+freeVar' (TApp f1 f2) = (freeVar' f1) `S.union` (freeVar' f2)
+freeVar' (PApp f1 f2) = (freeVar' f1) `S.union` (freeVar' f2)
+freeVar' (Forall x f) = S.delete x (freeVar' f)
+freeVar' (Lambda x (Just f1) f) =
+  S.delete x $ freeVar' f `S.union` freeVar' f1
+freeVar' (Lambda x Nothing f) =
+  S.delete x $ freeVar' f   
+freeVar' (Abs x f) = S.delete x (freeVar' f)
+freeVar' (Imply b h) = freeVar' b `S.union` freeVar' h
+
+freeKVar :: Kind -> S.Set Name
+freeKVar Star = S.empty
+freeKVar (KVar x) = S.insert x S.empty
+freeKVar (KArrow f1 f2) = (freeKVar f1) `S.union` (freeKVar f2)
+
+-- flatten of type
+flatten :: Exp -> [Exp]
+flatten (PApp f1 f2) = flatten f1 ++ [f2]
+flatten (App f1 f2) = flatten f1 ++ [f2]
+flatten (TApp f1 f2) = flatten f1 ++ [f2]
+flatten a = [a]
+getHead a = head $ flatten a
+getArgs a = tail $ flatten a
+reApp (y:ys) = foldl (\ z x -> App z x) y ys
+flattenK :: Kind -> [Kind]
+flattenK (KArrow f1 f2) =  f1 : flattenK f2
+flattenK a = [a]
+
+flattenF (Imply f1 f2) =  f1 : flattenF f2
+flattenF a = [a]
+
+-- convert eigenvariable to variable 
+rebind :: Exp -> Exp
+--rebind exp | trace ("rebind " ++show (exp)) False = undefined
+rebind (Const x) | isUpper $ head x = Const x
+                 | otherwise = (Var x)
+rebind (Var x) = Var x
+rebind (App t1 t2) = App (rebind t1) (rebind t2)
+rebind (TApp t1 t2) = TApp (rebind t1) (rebind t2)
+rebind (PApp t1 t2) = PApp (rebind t1) (rebind t2)
+rebind (Imply t1 t2) = Imply (rebind t1) (rebind t2)
+rebind (Lambda x Nothing t) =
+  Lambda x Nothing (rebind t)
+rebind (Lambda x (Just t') t) =
+  Lambda x (Just $ rebind t') (rebind t)
+rebind (Abs x t) = Abs x (rebind t)
+rebind (Forall x t) = Forall x (rebind t)
+rebind a = error $ show a
+  
+type BindCxt a = Reader [(Name, Int)] a
+
+-- debruijn representation of type 
+debruijn :: Exp -> BindCxt Nameless
+debruijn (Const x) = return $ C x
+
+debruijn (Var x) = do
+  n' <- asks (lookup x)
+  case n' of
+    Just n -> return $ V n
+    Nothing -> error $ show x ++ "what"
+
+
+debruijn (Forall x f) = do 
+  a <- local (((x,0):) . plus1) $ debruijn f 
+  return $ ALL a
+  
+debruijn (Imply f1 f2) = do
+  a1 <- debruijn f1
+  a2 <- debruijn f2
+  return $ IMP a1 a2
+
+debruijn (PApp b1 b2) = do
+  a <- debruijn b1
+  a1 <- debruijn b2
+  return $ AP a a1
+
+debruijn (Abs x f) = do
+  a <- local (((x,0):) . plus1) $ debruijn f 
+  return $ LAM a
+
+plus1 = map $ \(x, y) -> (x, y + 1)
+
+-- alphaEq of two type
+alphaEq :: Exp -> Exp -> Bool
+alphaEq t1 t2 =
+    let t1' = foldl' (\t x -> Forall x t) t1 (free t1)
+        t2' = foldl' (\t x -> Forall x t) t2 (free t2) in
+    runReader (debruijn t1') [] == runReader (debruijn t2') []
+
+
+
+type GVar a = State Int a
+
+type Subst = [(Name, Exp)]
+-- kind level sub
+applyK :: [(Name, Kind)] -> Kind -> Kind
+applyK s Star = Star
+applyK s Formula = Formula
+applyK s (KVar x) = case lookup x s of
+                       Just t -> t
+                       Nothing -> (KVar x)
+applyK s (KArrow k1 k2) = KArrow (applyK s k1) (applyK s k2)
+-- both type level and evidence level substitution
+applyE :: Subst -> Exp -> Exp
+applyE [] e = e
+applyE ((n,t):xs) e = applyE xs $ runSubst t (Var n) e
+
+runSubst :: Exp -> Exp -> Exp -> Exp
+runSubst t x t1 = fst $ runState (subst t x t1) 0
+  
+subst :: Exp -> Exp -> Exp -> GVar Exp
+subst s (Var x) (Const y) = return $ Const y
+
+subst s (Var x) (Var y) =
+  if x == y then return s else return $ Var y
+                               
+subst s (Var x) (Imply f1 f2) = do
+  c1 <- subst s (Var x) f1
+  c2 <- subst s (Var x) f2
+  return $ Imply c1 c2
+
+subst s (Var x) (App f1 f2) = do
+  c1 <- subst s (Var x) f1
+  c2 <- subst s (Var x) f2
+  return $ App c1 c2
+
+subst s (Var x) (TApp f1 f2) = do
+  c1 <- subst s (Var x) f1
+  c2 <- subst s (Var x) f2
+  return $ TApp c1 c2
+
+subst s (Var x) (PApp f1 f2) = do
+  c1 <- subst s (Var x) f1
+  c2 <- subst s (Var x) f2
+  return $ PApp c1 c2
+
+subst s (Var x) (Forall a f) =
+  if x == a || not (x `elem` free f) then return $ Forall a f
+  else if not (a `elem` free s)
+       then do
+         c <- subst s (Var x) f
+         return $ Forall a c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (Var (a++ show n)) (Var a) f
+         subst s (Var x) (Forall (a ++ show n) c1)
+
+subst s (Var x) (Abs a f) =
+  if x == a || not (x `elem` free f) then return $ Abs a f
+  else if not (a `elem` free s)
+       then do
+         c <- subst s (Var x) f
+         return $ Abs a c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (Var (a++ show n)) (Var a) f
+         subst s (Var x) (Abs (a ++ show n) c1)
+
+subst s (Var x) (Lambda a Nothing f) =
+  if x == a || not (x `elem` free f) then return $ Lambda a Nothing f
+  else if not (a `elem` free s)
+       then do
+         c <- subst s (Var x) f
+         return $ Lambda a Nothing c
+       else do
+         n <- get
+         modify (+1)
+         c1 <- subst (Var (a++ show n)) (Var a) f
+         subst s (Var x) (Lambda (a ++ show n) Nothing c1)         
+
+
+subst p (Var x) (Lambda y (Just t) p1) =
+  if x == y || not (x `elem` free p1)
+    then do
+    t1 <- subst p (Var x) t
+    return $ Lambda y (Just t1) p1
+    else if not (y `elem` free p)
+         then do
+           t1 <- subst p (Var x) t
+           c <- subst p (Var x) p1
+           return $ Lambda y (Just t1) c
+         else do
+           n <- get
+           modify (+1)
+           c1 <- subst (Var (y++ show n)) (Var y) p1
+           subst p (Var x) (Lambda (y++ show n) (Just t) c1)
+
+normEvidence (Var y) = (Var y)
+normEvidence (Const y) = (Const y)
+normEvidence (Imply f1 f2) = Imply (normEvidence f1)
+                             (normEvidence f2)
+
+normEvidence (App f1 f2) = App (normEvidence f1)
+                           (normEvidence f2)
+normEvidence (TApp f1 f2) = TApp (normEvidence f1)
+                            (normalize f2)
+
+normEvidence a@(PApp f1 f2) = normalize a
+normEvidence a@(Abs b f2) = normalize a
+normEvidence (Forall a f2) = Forall a $ normalize f2
+normEvidence (Lambda a Nothing f) = Lambda a Nothing (normEvidence f)
+normEvidence (Lambda y (Just t) p1) = Lambda y (Just $ normalize t) (normEvidence p1)
+-- normalize type expresion
+normalize :: Exp -> Exp
+-- normalize r | trace ("normalize " ++ show r) False = undefined
+normalize t = let t1 = norm t
+                  t2 = norm t1
+              in if t1 == t2 then t1 else normalize t2 -- `alphaEq`
+                                                 
+norm (Var a) = Var a
+norm (Const a) = Const a
+norm (Abs x t) = Abs x (norm t)
+norm (PApp (Abs x t') t) = runSubst t (Var x) t'
+norm (PApp (Var x) t) = PApp (Var x) (norm t)
+norm (PApp (Const x) t) = PApp (Const x) (norm t)
+norm (PApp t' t) = 
+  case (PApp (norm t') (norm t)) of
+    a@(PApp (Abs x t') t) -> norm a
+    b -> b
+norm (Imply t t') = Imply (norm t) (norm t')
+norm (Forall x t) = Forall x (norm t)
+
+  
