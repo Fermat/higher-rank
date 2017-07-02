46c78f6cfb4f9c44b1304a382d55c28b6354dede
fermat <fermat1217@gmail.com>
2017-07-02 18:24:32 -0300

    I think I have to use list of success pattern

--- src/Matching.hs
+++ src/Matching.hs
@@ -9,14 +9,19 @@ import Control.Monad.Except
 -- substitution
 type Subst = [(String, Exp)]
 
-data MatchState = MatchState {subst :: Subst, counter :: Int }
+-- As we have second-order matching, the state will be a list of
+-- all possible success substitutions
+data MatchState = MatchState {subst :: [Subst], counter :: Int }
                 deriving (Show)
 
 updateSubst :: Subst -> MatchState -> MatchState
-updateSubst sub s@(MatchState{subst}) = s{subst = extend sub subst}
+updateSubst sub s@(MatchState{subst}) = s{subst = map (extend sub) subst}
 
 extend :: Subst -> Subst -> Subst
 extend s1 s2 = [(x, apply s1 e) | (x, e) <- s2] ++ s1
+
+-- fresh assumption: I am assuming the domain of the substitution
+-- to be fresh variables.
 
 apply :: Subst -> Exp -> Exp
 apply s (Var x) = case lookup x s of
@@ -25,8 +30,7 @@ apply s (Var x) = case lookup x s of
 apply s a@(Const _) = a
 apply s (App f1 f2) = App (apply s f1) (apply s f2)
 apply s (Imply f1 f2) = Imply (apply s f1) (apply s f2)
-apply s (Forall x f2) = Imply (apply s f1) (apply s f2)
-
+apply s (Forall x f2) = Forall x (apply s f2)
 
   
 newtype MatchMonad a = MatchMonad {runM :: StateT MatchState (Except Doc) a }
@@ -39,7 +43,7 @@ runMatchMonad s a = runExcept $ runStateT (runM a) s
 
 initMatchState = MatchState [] 0
 
-match :: Exp -> Exp -> MatchMonad ()
-match (Var x) e = if x `elem` freeVars e then
-                    throwError $ text "variable" <+> text x <+> text "in" <+> disp e
-                  else do 
+-- match :: Exp -> Exp -> MatchMonad ()
+-- match (Var x) e = if x `elem` freeVars e then
+--                     throwError $ text "variable" <+> text x <+> text "in" <+> disp e
+--                   else do 
