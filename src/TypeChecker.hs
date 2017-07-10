db265953b9720697befba725cfe9d42f4ee85c79
Frank Fu <peng.frank.fu@gmail.com>
2017-07-10 17:19:20 -0300

    need to deal with kinding first

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,2 +1,15 @@
 module TypeChecker where
 
+import Syntax
+import Pretty
+import Matching
+
+type KindDef = [(Name, Exp)]
+
+-- Well-formed kinds
+wfKind :: Exp -> Bool
+wfKind Star = True
+wfKind (Imply x y) = wfKind x && wfKind y
+
+
+
