617b7849288600081bb76d0f3813ede4152edd41
Frank Fu <peng.frank.fu@gmail.com>
2017-07-11 12:04:25 -0300

    begin the quest of defining the transition function

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -3,4 +3,34 @@ module TypeChecker where
 import Syntax
 import Pretty
 import Matching
+import KindChecker
 
+import Text.PrettyPrint
+
+
+type Pos = [Int] 
+
+-- Global type environment
+type TyEnv = [(Name, Exp)]
+
+
+data Phi = Phi{
+              position :: Pos,
+              currentGoal :: Exp,
+              currentProg :: Exp,
+              env :: TyEnv,
+              scope :: [Name] }
+           deriving (Show)
+
+-- Resolution state
+data ResState = Res{
+  kindDefs :: KindDef,
+  fun :: Name,
+  mixedTerm :: Exp,
+  phi :: [Phi],
+  errMessage :: Maybe Doc,
+  counter :: Int
+  } deriving (Show)
+
+transit :: ResState -> [ResState]
+transit = undefined
