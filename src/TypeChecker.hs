8bdf99db311edea5b269aba4089f4b83fc6374ae
peng fu <fermat1217@gmail.com>
2017-07-13 17:57:17 -0300

    even lambda case is tricky

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -13,7 +13,6 @@ type Pos = [Int]
 -- Global type environment
 type TyEnv = [(Name, Exp)]
 
-
 data Phi = Phi{
               position :: Pos,
               currentGoal :: Exp,
@@ -32,5 +31,28 @@ data ResState = Res{
   counter :: Int
   } deriving (Show)
 
+getHB ::  Exp -> ([Exp],Exp)
+getHB (Imply x y) = let (bs, t') = getHB y in (x:bs, t')
+getHB t = ([], t)
+
+patternVars :: Exp -> Int -> (TyEnv, Int)
+patternVars p i = let fvs = freeVars p
+                      j = (i+(length fvs))-1
+                      ns = [i..j]
+                      vars = map (\ n -> Var $ "y"++show n++"'") ns in
+                    (zip fvs vars, j)
+
 transit :: ResState -> [ResState]
-transit = undefined
+transit (Res ks f pf ((Phi pos goal@(Imply _ _) exp@(Lambda _ _ _) gamma lvars):phi) Nothing i) =
+  let (bs, h) = getHB goal
+      (vars, b) = (map fst $ viewLArgs exp, viewLBody exp) in
+      if length vars < length bs then
+        let m' = Just $
+                   text "arity mismatch when handling lambda abstraction" $$
+                   (nest 2 (text "current goal: " <+> disp goal)) $$ nest 2
+                   (text "current program:"<+> disp exp) $$
+                   (nest 2 $ text "current mixed proof term" $$ nest 2 (disp pf)) in
+          [(Res ks f pf ((Phi pos goal exp gamma lvars):phi) m' i)]
+      else undefined 
+  
+
