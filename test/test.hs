b0dcd9b4d6ea29bee1a0893d36a9a6d48a0ecdba
peng fu <fermat1217@gmail.com>
2017-08-24 15:15:47 -0300

    church encoding examples

--- test/test.hs
+++ test/test.hs
@@ -18,6 +18,17 @@ l' = let y :: forall x . F x
          y = fix l
      in what y
 
+type Nat = forall x . (x -> x) -> x -> x
+
+zero :: Nat 
+zero = \ s z -> z
+
+suc :: Nat -> Nat
+suc n = \ s z -> s (n s z)
+
+add :: Nat -> Nat -> Nat
+add n m = n suc m
+
 -- test :: Void
 -- test = what (fix l'')
 
